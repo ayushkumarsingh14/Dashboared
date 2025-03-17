1adc4c822b076657a23a8271d523b457473e9aed
Ayush Kumar Singh <aaks.feb14@gmail.com>
2025-03-17 23:12:24 +0530

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import "./globals.css"; 
+
+
+export default function Home() {
+  console.log(Button)
+  return (
+    <main >
+      
+      <Button variant="outline" size="lg">
+        Click 
+      </Button>
+    </main>
+  );
+}
