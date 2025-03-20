f23557862f80f27b6e38940497b9a2d285cedad4
Ayush Kumar Singh <aaks.feb14@gmail.com>
2025-03-20 23:33:48 +0530

    Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,37 +7,48 @@ import {
 import UserItem from "./UserItem";
 
 export default function Sidebar() {
+  const menuList = [
+    {
+      group: "General",
+      items: [
+        { link: "/", text: "Profile" },
+        { link: "/", text: "Inbox" },
+        { link: "/", text: "Billing" },
+        { link: "/", text: "Notifications" }
+      ]
+    },
+    {
+      group: "Settings",
+      items: [
+        { link: "/", text: "General Settings" },
+        { link: "/", text: "Privacy" },
+        { link: "/", text: "Logs" }
+      ]
+    }
+  ];
+
   return (
     <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
-      <UserItem />  {/* No need for extra div */}
+      <UserItem />
 
       <div className="grow">
         <Command>
           <CommandInput placeholder="Type a command or search..." />
           <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            
-            <CommandGroup heading="Suggestions">
-              <CommandItem>Calendar</CommandItem>
-              <CommandItem>Search Emoji</CommandItem>
-              <CommandItem>Calculator</CommandItem>
-            </CommandGroup>
+            {menuList.map((menu, key) => (
+              <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option, optionsKey) => (
+                  <CommandItem key={optionsKey}>{option.text}</CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
 
             <CommandSeparator />
-
-            <CommandGroup heading="Settings">
-              <CommandItem>Profile</CommandItem>
-              <CommandItem>Billing</CommandItem>
-              <CommandItem>Settings</CommandItem>
-            </CommandGroup>
           </CommandList>
         </Command>
       </div>
 
-      <div className="text-center text-gray-500 mt-4"> {/* Style for better UI */}
-        Notification
-      </div>
-      
+      <div className="text-center text-gray-500 mt-4">Notification</div>
     </div>
   );
 }
